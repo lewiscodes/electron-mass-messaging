6baacaee7e5c28c2e9dfc137522b8a80424c551e
Lewis Turner <lewis.turner@lewiss-mbp.home>
2020-10-29 11:27:29 +0000

    style map points based on resource status and type

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -7,7 +7,7 @@ import { Map, View, MapBrowserEvent, Feature } from 'ol';
 import { setResources } from '../../store/actions/resources';
 import { IResource } from '../../store/types/resources';
 import { IReduxStore } from '../../store/types';
-import {Circle as CircleStyle, Fill, Style} from 'ol/style';
+import {Circle as CircleStyle, Fill, RegularShape, Stroke, Style} from 'ol/style';
 import {OSM, Vector as VectorSource} from 'ol/source';
 import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
 import Point from 'ol/geom/Point';
@@ -18,7 +18,6 @@ const MapPage = ({ children }: { children: JSX.Element }) => {
     const resources = useSelector((state: IReduxStore) => state.resources.items);
     const [map, setMap] = useState<Map>();
     const [vectorLayer, setVectorLayer] = useState<VectorLayer>()
-
 
     useEffect(() => {
         ipcRenderer.send('RESOURCES_SUBSCRIBE');
@@ -31,15 +30,7 @@ const MapPage = ({ children }: { children: JSX.Element }) => {
 
         const source = new VectorSource();
         const vector = new VectorLayer({
-            source: source,
-            style: new Style({
-                image: new CircleStyle({
-                    radius: 3,
-                    fill: new Fill({
-                        color: 'red',
-                    }),
-                }),
-            }),
+            source: source
         });
 
         const map: Map = new Map({
@@ -71,20 +62,13 @@ const MapPage = ({ children }: { children: JSX.Element }) => {
     useEffect(() => {
         const source = new VectorSource();
         const vector = new VectorLayer({
-            source: source,
-            style: new Style({
-                image: new CircleStyle({
-                    radius: 5,
-                    fill: new Fill({
-                        color: 'red',
-                    }),
-                })
-            }),
+            source: source
         });
 
         resources.forEach(resource => {
             const point = new Point(resource.location);
             const feature = new Feature(point);
+            feature.setStyle(calculateResourceStyle(resource));
             source.addFeature(feature);
         });
 
@@ -92,6 +76,70 @@ const MapPage = ({ children }: { children: JSX.Element }) => {
         map?.addLayer(vector);
         setVectorLayer(vector);
     }, [resources]);
+
+    const calculateResourceStyle = (resource: IResource) => {
+        const color = calculateResourceColor(resource);
+        const fill = new Fill({color});
+
+        switch (resource.type) {
+            case 'CID':
+                return new Style({
+                    image: new RegularShape({
+                        fill,
+                        points: 4,
+                        radius: 10,
+                        angle: Math.PI / 4,
+                    })
+                });
+            case 'RC':
+                return new Style({
+                    image: new RegularShape({
+                        fill,
+                        points: 3,
+                        radius: 10,
+                        rotation: Math.PI / 4,
+                        angle: 0,
+                    })
+                });
+            case 'PCSO':
+                return new Style({
+                    image: new RegularShape({
+                        fill,
+                        points: 5,
+                        radius: 10,
+                        radius2: 4,
+                        angle: 0,
+                    })
+                });
+            case 'COD':
+                return new Style({
+                    image: new CircleStyle({
+                        radius: 5,
+                        fill,
+                    })
+                });
+            case 'CIS':
+                return new Style({
+                    image: new CircleStyle({
+                        radius: 5,
+                        fill,
+                    })
+                })
+        }
+    }
+
+    const calculateResourceColor = (resource: IResource) => {
+        switch (resource.status) {
+            case '05':
+                return 'red'
+            case '02':
+                return 'blue'
+            case '06':
+                return 'green'
+            case 'AS':
+                return 'orange'
+        }
+    };
 
     return (
         <div
